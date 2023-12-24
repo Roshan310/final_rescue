a1e9ebb3f8bc453e191851ee7b48f79962e45a55
Roshan310 <yuvrajaryal83@gmail.com>
2023-12-24 09:40:18 +0545

    initial

--- backendapi/app/database.py
+++ backendapi/app/database.py
@@ -1,55 +1,19 @@
-import requests
-from bs4 import BeautifulSoup
-# import sqlite3
-# from models import News
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# con = sqlite3.connect('news.db')
-# cur = con.cursor()
+DATABASE_URL = "sqlite:///./sql_app.db"
 
-def get_all_news():
-  url = ('https://www.onlinekhabar.com/?search_keyword=disaster&fbclid=IwAR3iFqwtj-DVIkd7GHxWqa9eSAHxMkv-ozK2Y8BLCQQK2kXhD5a7yfpPgZE')
-  r = requests.get(url)
-  html_content = r.content
-  soup = BeautifulSoup(html_content, "html.parser")
-  divs = soup.find_all('div', class_='ok-news-post ok-post-ltr')
-  # print(soup.p)
-  # print(divs)
-  news = []
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-  for div in divs:
-      news_link = soup.find('a')['href']
-      news_title = div.find("div", class_="ok-post-content-wrap").h2.text		
-      news_info = div.find("div", class_="ok-title-info").p.text
-      
-      news_dict = [
-      news_title,
-      news_link,
-      news_info
-      ]
-      news.append(news_dict)
-  return news
+Base = declarative_base()
 
-# all_news = get_all_news()
-# for news in all_news:
-#    print(news[0])
-# for news in all_news:
-#    qry = f"""INSERT INTO news (news_link, news_title, news_info)
-#         VALUES ('{news[0]}', '{news[1]}', '{news[2]}')
-#     """
-#    cur.execute(qry)
-#    con.commit()
-#    con.close()
-   
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+    
 
-
-# for one_news in news:
-#     news = News(news_title=one_news[0], news_link=one_news[1], news_info=one_news[2])
-#     db.commit()
-    # sql = '''INSERT INTO `nepali_news`
-    #         ( `title`, `link`, `summary`) 
-    #         VALUES (?, ?, ?)'''
-    # cur.executemany(sql,news)
-    # con.commit()
-    # con.close()
-
-	
